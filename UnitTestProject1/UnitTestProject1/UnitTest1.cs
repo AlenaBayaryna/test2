7537fca5eecc3804ca60ff90b9cb75c0fc81ea1a
Alena <alenabayaryna@gmail.com>
2018-09-04 10:00:34 +0300

    1111

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
